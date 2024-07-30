4629f7e28a2b2f3793060d8e1526f78283d19e82
Gurpreet Singh <gurpranked@Razer-Blade-14>
2024-07-30 16:49:54 -0400

    chore: added web_docs for website

---
+++ docs/web_docs/mercury_script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# cd ~/UMLCloudComputing.github.io
+git pull origin main
+git submodule foreach git pull origin main
+cp submodules/UniPath.io/docs/web_docs/* docs/projects/unipath
+cp submodules/rowdybot/docs/web_docs/* docs/projects/rowdybot
+npm run build
+npm run serve
